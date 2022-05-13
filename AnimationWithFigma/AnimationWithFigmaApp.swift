fcf1f2075e4352ee4585fdb588766f3eb24b1a07
Mahdi <97763355+MHanifeh@users.noreply.github.com>
2022-05-13 15:57:25 +0430

    Initial Commit

---
+++ AnimationWithFigma/AnimationWithFigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationWithFigmaApp.swift
+//  AnimationWithFigma
+//
+//  Created by MahdiHanifeh on 2/23/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct AnimationWithFigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationWithFigma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationWithFigma
+//
+//  Created by MahdiHanifeh on 2/23/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
